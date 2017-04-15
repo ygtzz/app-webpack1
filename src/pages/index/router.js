cf675d058a509d543f238229464488a2642d864c
ygtzz <ygtzz@126.com>
2017-04-15 17:14:46 +0800

    add route split code

--- src/pages/index/router.js
+++ src/pages/index/router.js
@@ -1,14 +1,60 @@
-import me from './views/me.vue';
-import productList from './views/productList.vue';
-import detail from './views/detail.vue';
-import home from './views/home.vue';
-import test from './views/test.vue';
+// import me from './views/me.vue';
+// import productList from './views/productList.vue';
+// import detail from './views/detail.vue';
+// import home from './views/home.vue';
+// import test from './views/test.vue';
+
+const me = resolve => {
+  require.ensure(['./views/me.vue'], () => {
+    resolve(require('./views/me.vue'))
+  },'me')
+}
+const productList = resolve => {
+  require.ensure(['./views/productList.vue'], () => {
+    resolve(require('./views/productList.vue'))
+  },'producsList')
+}
+const detail = resolve => {
+  require.ensure(['./views/detail.vue'], () => {
+    resolve(require('./views/detail.vue'))
+  },'detail')
+}
+const home = resolve => {
+  require.ensure(['./views/home.vue'], () => {
+    resolve(require('./views/home.vue'))
+  },'home')
+}
+const test = resolve => {
+  require.ensure(['./views/test.vue'], () => {
+    resolve(require('./views/test.vue'))
+  },'test')
+}
 
 export default [
     { path: '/', redirect:'/home'},
-    { path: '/home', component: home, name:'home'},            
-    { path: '/me', component: me, name:'me'},    
-    { path: '/finance', component: productList, name:'finance'},
-    { path: '/detail', component: detail, name:'detail'},
-    { path: '/test', component: test,name:'test'}
+    { 
+        path: '/home', 
+        name:'home',
+        component: home
+    },            
+    { 
+        path: '/me', 
+        name:'me',
+        component: me
+    },    
+    { 
+        path: '/finance', 
+        name:'finance',
+        component: productList        
+    },
+    { 
+        path: '/detail', 
+        name:'detail',
+        component: detail        
+    },
+    { 
+        path: '/test', 
+        name:'test',
+        component: test        
+    }
 ];
